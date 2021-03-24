43d7a2575d84aefa47ec4021243ed6efb0edb054
Benny Goller <gollerb@purdue.edu>
2021-03-24 10:58:41 -0400

    changed structure of population modeling to keep track of a lake population and count time in years
    added option of evolving a different age to maturation

--- Pink Salmon/Simple IBM attempt/BG salmon IBM.R
+++ Pink Salmon/Simple IBM attempt/BG salmon IBM.R
@@ -1,5 +1,5 @@
 ##################################################
-#Last edited by B. Goller on 10 March 2021
+#Last edited by B. Goller on 22 March 2021
 #Set up simple salmon IBM
 ##################################################
 #Get the required packages
@@ -26,11 +26,20 @@ reproduction <- function(return.fish, percent.repro, g.rate){
 nextgen.fish <- function(spawn.pop){
   nextgen <- data.frame(matrix(NA, sum(spawn.pop$num.f1), ncol(spawn.pop)))
   colnames(nextgen) <- colnames(spawn.pop)
+  nextgen$year <- rep(spawn.pop$year[1], nrow(nextgen))
   nextgen$fish.num <- c(1:nrow(nextgen))+max(spawn.pop$fish.num)
+  nextgen$age <- rep(0, nrow(nextgen))
+  
+  #SELECT ONE OPTION TO DETERMINE WHETHER AGE AT MATURATION IS FIXED
+  #if prob = c(100,0) then there is no chance of developing a different maturation time
+  nextgen$age.mat <- sample(c(2,3), nrow(nextgen), prob = c(100,0), replace = TRUE)
+  #allow some fish to evolve a 3-yr maturation if probability of selecting a 3 is >0
+  #nextgen$age.mat <- sample(c(2,3), nrow(nextgen), prob = c(95,5), replace = TRUE)
+  
   #give offspring their parent's mass
-  nextgen$mass <- unlist(apply(X = spawn.pop[,c(2,3)], MARGIN = 1, function(X) rep(X[1], X[2])))
+  nextgen$mass <- unlist(apply(X = spawn.pop[,c(3,6)], MARGIN = 1, function(X) rep(X[1], X[2])))
   #keep track of parent fish.num
-  nextgen$parent.num <- unlist(apply(X = spawn.pop[,c(1,3)], MARGIN = 1, function(X) rep(X[1], X[2])))
+  nextgen$parent.num <- unlist(apply(X = spawn.pop[,c(2,6)], MARGIN = 1, function(X) rep(X[1], X[2])))
   return(nextgen)
 }
 ##################################################
@@ -59,14 +68,66 @@ habitat.K <- 5000
 gen.growth <- 1.2
 #will say that only half the fish reproduce
 p.repro <- 0.5
+yr.start <- 1970
 
 
-salmon.df.init <- data.frame(matrix(NA, pop.initial, 4))
-colnames(salmon.df.init) <- c("fish.num", "mass", "num.f1","parent.num")
+salmon.df.init <- data.frame(matrix(NA, pop.initial, 7))
+colnames(salmon.df.init) <- c("year", "fish.num", "mass", "age", "age.mat", "num.f1","parent.num")
+salmon.df.init$year <- rep(yr.start,nrow(salmon.df.init))
 salmon.df.init$fish.num <- c(1:pop.initial)
+salmon.df.init$age <- rep(0,nrow(salmon.df.init))
+salmon.df.init$age.mat <- rep(2,nrow(salmon.df.init))
 salmon.df.init$mass <- fatten.fish(salmon.df.init, 8, 1)
-salmon.df.init$num.f1 <- reproduction(salmon.df.init, p.repro, gen.growth)
+#salmon.df.init$num.f1 <- reproduction(salmon.df.init, p.repro, gen.growth)
 
+
+
+#set up a lake population
+#fish from initial population swim out to the lake
+lake.fishes <- salmon.df.init
+lake.survey <- NULL
+lake.survey[[1]] <- lake.fishes
+
+for(i in c(1:50)){
+  #clear the babies variable to make sure we don't accidentally carry over fish from last year
+  babies <- NULL
+  
+  #fish in lake spend 1 year growing
+  lake.fishes$year <- lake.fishes$year + 1
+  lake.fishes$age <- lake.fishes$age + 1
+  print(lake.fishes$year[1])
+  
+  #after that year, do a reproductive age check
+  spawners <- which(lake.fishes$age == lake.fishes$age.mat)
+  
+  #only allow spawning if there are at least 10 fishes returning to spawn
+  #otherwise fishes that reach reproductive age but don't have mates just die
+  ifelse(length(spawners) > 9,
+         {lake.fishes$num.f1[spawners] <- reproduction(lake.fishes[spawners,], p.repro, gen.growth);
+          #get a data frame with all the babies that are leaving the stream and entering the lake
+          babies <- nextgen.fish(lake.fishes[spawners,]);
+          #let the babies swim out into the lake and join the non-spawner lake fish
+          lake.fishes <- rbind(lake.fishes[-spawners,], babies)},
+          ifelse(length(spawners) > 0,
+                 lake.fishes <- lake.fishes[-spawners,],
+                 lake.fishes <- lake.fishes))
+  
+  #store the snapshot of what the lake population looks like for the year
+  #post reproduction if any
+  lake.survey[[i+1]] <- lake.fishes
+}
+
+lake.fish.count <- matrix(NA, length(lake.survey), 2)
+for(a in 1:length(lake.survey)){
+  lake.fish.count[a,] <- c(a, nrow(lake.survey[[a]]))
+}
+plot(lake.fish.count[,1], lake.fish.count[,2])
+
+
+
+
+##################################################
+##################################################
 salmon.gens <- NULL
 salmon.gens[[1]] <- salmon.df.init
 salmon.curr.gen <- salmon.df.init
@@ -81,6 +142,9 @@ for(g in 2:100){
 }
 plot(salmon.abund[,1], salmon.abund[,2])
 plot(salmon.abund[,1], salmon.abund[,3])
+
+
+
 
 salmon.gens2 <- NULL
 salmon.gens2[[1]] <- salmon.df.init
