93707a51e3d62e42078aac2798357ff5cdf200c6
morgan-sparks <msparks1309@gmail.com>
2021-03-30 18:44:40 -0400

    the whole kitten kaboodle

--- Pink Salmon/Simple IBM attempt/MS_MultiPopIBM.R
+++ Pink Salmon/Simple IBM attempt/MS_MultiPopIBM.R
@@ -1,4 +1,4 @@
-
+library(ggplot2); library(patchwork)
 #------- initialize population
 
 ### make waterbodies 
@@ -26,12 +26,12 @@ birth_draw <- c(rep(1,50), rep(2,30), rep(3, 10), rep(0,10))
 #------- emigrate function
 emigrate <-  function(rivers){
   #test to see if anything in the river
-  if(is.null(rivers)==TRUE){
-    print(paste("No fish in", rivers))
+  if(nrow(rivers)==0){
+    print(paste("No fish in rivers"))
   } 
   # if there are fish in river then sample from their respective emigrant draw
   else{
-    if(rivers == "current_river"){
+    if(rivers[1,"mig_river"]=="current_river"){
       for(i in 1:nrow(rivers)) {
         rivers[i,"mig_river"] <- sample(curr_riv_emdraw, size = 1, replace = TRUE)
       }
@@ -52,12 +52,12 @@ outmigrate <-  function(current_river, wolf_river, lake){
   lake <- rbind(lake, current_river, wolf_river)
   
   #clear rivers
-  current_river <- NULL
-  wolf_river <- NULL
+  current_river <- current_river[FALSE,]
+  wolf_river <- wolf_river[FALSE,]
   
   #make and return waterbodies
   
-  waterbodies <- list(rivers = list(current_river, wolf_river), lake  = lake)
+  waterbodies <- list(rivers = list(current_river = current_river, wolf_river = wolf_river), lake  = lake)
   
   return(waterbodies)
   
@@ -88,61 +88,31 @@ migrate <- function(lake){
   lake[,"age"] <- lake[,"age"] + 1
   
   #remake and return waterbodies
-  waterbodies <- list(rivers = list(current_river, wolf_river), lake = lake)
+  waterbodies <- list(rivers = list(current_river = current_river, wolf_river = wolf_river), lake = lake)
   return(waterbodies)
   
 }
 
-
-#------- old ### migrate to lake and age (must take those fish and move)
-# migrate <- function(rivers, lake){
-#   
-#   #append current river fish into lake
-#   lake <- rbind(lake, rivers)
-#   
-#   #clear out rivers
-#   rivers <- NULL
-#   #make temp lake
-#   lake_temp <-NULL
-#   
-#   
-#   for(y in 1:nrow(lake)){
-#     
-#     # if age == age at maturity, put those fish back into current river
-#     if(lake[y,3] == lake[y,4]){
-#       
-#       #put fish whose age == age at maturity into current river
-#       rivers <- rbind(rivers, lake[y,])
-#       
-#     } else{
-#       # put fish whose age != age at maturity into temp lake
-#       lake_temp <-rbind(lake_temp, lake[y,])
-#     }
-#   }
-#   
-#   # make "lake" fish that are less that age at maturity
-#   lake <- lake_temp
-#   # add a year of age 
-#   lake[,"age"] <- lake[,"age"] + 1
-#   
-#   #create list and return it
-#   return(waterbodies)
-# }
 #------- density dependence
 
 density_dependence <- function(rivers){
   if(is.null(rivers)==TRUE){
     print(paste("No limitation on spawning, fish are in lake"))
   }else{
+    if(nrow(rivers)<20){ #if ther are less 20 fish in the river thet can't spawn and set pop = 0
+      rivers <- rivers[FALSE,]
+    }else{
     pop_size <- nrow(rivers) # make pop size = to length of pop
     k <-round(rnorm(1, mean = 5000, sd = 250),0) # K is a random draw from normal dist w/ mean 5000 and sd 250
     if(pop_size > k){
-      number_to_kill <- pop_size - k # get number of individuals to kill
+      number_to_kill <- 1.5*(pop_size - k) # get number of individuals to kill (1.5* # over K)
       individuals_to_kill <- sample(1:pop_size, size = number_to_kill, replace = FALSE)
       # kill individuals in current river that match those in individuals to kill
       rivers[individuals_to_kill,"death"]  <- 1
     }
+    }
   }
+  
   return(rivers)
 }
 
@@ -171,9 +141,13 @@ make_baby <- function(rivers){
 ### make waterbodies 
 
 # make list of rivers, only current river will have fish to start, everything else will be empty
-waterbodies <- list(rivers = list(current_river = data.frame(fish = 1:100, mig_river = "current_river", year = 1, age = 1, age_maturity = 2, emigrate = 0, birth = 0, death = 0, stringsAsFactors = FALSE),
-                                  wolf_river = NULL), lake = NULL)
 
+current_river <- data.frame(fish = 1:100, mig_river = "current_river", year = 1, age = 1, age_maturity = 2, emigrate = 0, birth = 0, death = 0, stringsAsFactors = FALSE)
+wolf_river <- current_river[FALSE,] # make empty df but inherit names from current river
+lake <- current_river[FALSE,] # make empty df but inherit names from current river
+
+waterbodies <- list(rivers = list(current_river = current_river, wolf_river = wolf_river),
+                    lake = lake)
 ### draw parameters for births, emigrants, etc
 
 # 10% chance of emigration
@@ -183,6 +157,8 @@ wolf_riv_emdraw <-  c(rep("wolf_river",90), rep("current_river", 10))
 #50% chance of replacement, 30% of doubling, 10% of tripling, 10% of not at all
 birth_draw <- c(rep(1,50), rep(2,30), rep(3, 10), rep(0,10))
 
+# age at maturyit draw
+#mat_age_draw <- c(rep(2,90), rep(3,10))
 # num of gens
 gens <- c(2:200)
 
@@ -218,7 +194,7 @@ for (j in gens){
   #------- compile summary statistics
   
   ### for current river
-  if(is.null(waterbodies$rivers[1]) == TRUE){
+  if(nrow(waterbodies$rivers$current_river) == 0){
     cr_N <- 0
     cr_births <- 0
   }else{
@@ -227,7 +203,7 @@ for (j in gens){
   }
   
   ### for wolf river
-  if(is.null(waterbodies$rivers[2]) == TRUE){
+  if(nrow(waterbodies$rivers$wolf_river) == 0){
     wr_N <- 0
     wr_births <- 0
   } else{
@@ -236,30 +212,30 @@ for (j in gens){
   }
   
   ### for lake
-  if(is.null(waterbodies$lake) == TRUE){
+  if(nrow(waterbodies$lake) == 0){
     lake_N <- 0
   } else{
   lake_N <- nrow(waterbodies$lake)
   }
   
-  sum_stats <- rbind(sum_stats, cbind(cr_N, cr_births, wr_N, wr_births, lake_N))
+  sum_stats <- rbind(sum_stats, cbind(year = j,cr_N, cr_births, wr_N, wr_births, lake_N))
   
   #------- remake waterbodies with new births
   
   ### current river
-  if(is.null(waterbodies$rivers$current_river)== TRUE){
-    current_river <- NULL
+  if(nrow(waterbodies$rivers$current_river)== 0){
+    current_river <- waterbodies$rivers$current_river
   }else{
-  current_river <- data.frame(fish = sum(waterbodies$rivers$current_river[,"birth"]), 
+  current_river <- data.frame(fish = 1:sum(waterbodies$rivers$current_river[,"birth"]), 
                               mig_river = "current_river", year = j, age = 1, age_maturity = 2, 
                               emigrate = 0, birth = 0, death = 0, stringsAsFactors = FALSE)
   }
   
   ### wolf river
-  if(is.null(waterbodies$rivers$wolf_river)== TRUE){
-    wolf_river <- NULL
+  if(nrow(waterbodies$rivers$wolf_river) == 0){
+    wolf_river <- waterbodies$rivers$wolf_river
   }else{
-    wolf_river <- data.frame(fish = sum(waterbodies$rivers$wolf_river[,"birth"]), 
+    wolf_river <- data.frame(fish = 1:sum(waterbodies$rivers$wolf_river[,"birth"]), 
                                 mig_river = "wolf_river", year = j, age = 1, age_maturity = 2, 
                                 emigrate = 0, birth = 0, death = 0, stringsAsFactors = FALSE)
   }
@@ -269,4 +245,49 @@ for (j in gens){
   
 
 }
+
+
+#####################################################################
+
+# plot IBM
+
+#####################################################################
+
+sum_stats <- data.frame(sum_stats)
+
+y200 <- ggplot(data = sum_stats)+
+ geom_line(aes(x = year, y = cr_N, color = "Current River"), size =0.5) +
+  geom_line(aes(x =year, y = wr_N, color = "Wolf River"), size =0.5 ) +
+  geom_hline(yintercept = 5000, linetype = "dashed" ) +
+  scale_color_manual(name = "Population", 
+                     values = c("Current River" = "dodgerblue", "Wolf River" = "orange")) +
+  labs( x ="Year", y = "Population size", title= "Years 1-200") +
+  theme_classic(base_size = 14) +
+  theme(legend.position = "none") 
+
+y10 <- ggplot(data = sum_stats[1:10,])+
+  geom_line(aes(x = year, y = cr_N, color = "Current River"), size =1) +
+  geom_line(aes(x =year, y = wr_N, color = "Wolf River"), size =1 ) +
+  #geom_hline(yintercept = 5000, linetype = "dashed" ) +
+  scale_color_manual(name = "Population", 
+                     values = c("Current River" = "dodgerblue", "Wolf River" = "orange")) +
+  labs( x ="Year", y = "Population size", title= "Years 1-10") +
+  theme_classic(base_size = 14) +
+  theme(legend.position = "none") 
+
+y50 <- ggplot(data = sum_stats[1:50,])+
+  geom_line(aes(x = year, y = cr_N, color = "Current River"), size =0.5) +
+  geom_line(aes(x =year, y = wr_N, color = "Wolf River"), size =0.5 ) +
+  geom_hline(yintercept = 5000, linetype = "dashed" ) +
+  scale_color_manual(name = "Population", 
+                     values = c("Current River" = "dodgerblue", "Wolf River" = "orange")) +
+  labs( x ="Year", y = "Population size", title= "Years 1-50") +
+  theme_classic(base_size = 14) +
+  theme(legend.position = "none") 
+
+(y10 + y50)/y200 + theme(legend.position = "bottom")
+
+
+
+
 
