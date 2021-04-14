a1a4d57aeec897957da95878bfbd51abe36f56b0
Benny Goller <gollerb@purdue.edu>
2021-04-14 13:55:27 -0400

    set up the data structure for the lake

---
+++ Pink Salmon/model v.1/lake population v1.R
@@ -0,0 +1,48 @@
+#Data structure to hold all fish currently in the lake population
+
+#Data structure to hold the habitat information
+habitat <- data.frame(river = c("Current", "Wolf", "Steel"), 
+                      pop.k = c(500, 500, 2000), 
+                      migr.curr = c(95,5,0), 
+                      migr.wolf = c(5,90,5), 
+                      migr.steel = c(0,5,95))
+#Alternatively, have the habitat variants be their own .csv file
+#habitat <- read.csv("filename.csv", header = TRUE)
+
+#set up the data frame for the lake population
+#start with some initial colonizing population (those first released into lake)
+pop.initial <- 100
+lake.salmon <- data.frame(matrix(NA, pop.initial, 11))
+colnames(lake.salmon) <- c("year", "fish.num", "sex", "mass", "age", "age.mat", "river", "migrant", "num.f1", "mom.num", "dad.num")
+
+#what year were the fish released?
+lake.salmon$year <- rep(1950, nrow(lake.salmon))
+
+#number founding population with 1:pop.initial
+lake.salmon$fish.num <- c(1:pop.initial)
+
+#sex based on a ratio
+p.male <- 0.5
+#randomly sampled sex
+lake.salmon$sex <- factor(sample(x = c("M","F"), size = pop.initial, replace = TRUE, prob = c(p.male, 1-p.male)))
+#non-random sex ratio
+#lake.salmon$sex <- factor(c(rep("M", round(pop.initial*p.male)), rep("F", pop.initial-round(pop.initial*p.male))))
+
+#number founding population with 1:pop.initial
+#arbitrary...not sure if we want to keep track of masses
+lake.salmon$mass <- rnorm(nrow(lake.salmon), mean = 3, sd = 0.5)
+
+#age at release assumed to be 0
+lake.salmon$age <- rep(0,nrow(lake.salmon))
+#age at maturation of the released fish assumed to be 2
+lake.salmon$age.mat <- rep(2,nrow(lake.salmon))
+
+#river is spawning location for the released fish
+lake.salmon$river <- rep("Current",nrow(lake.salmon))
+#will need a line for handling factors to ensure that river is a factor 
+#with all possible rivers based on habitat
+#initial fish have not had the opportunity to migrate
+lake.salmon$migrant <- rep(0,nrow(lake.salmon))
+
+#initial fish have not had a breeding opportunity so leave num.f1 as 'NA'
+#similarly they are the original fish so we won't have info on their parents
