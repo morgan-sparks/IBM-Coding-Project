25650388865597221011b1acaddcc09e2ef1c00a
morgan-sparks <msparks1309@gmail.com>
2021-07-09 11:35:33 -0400

    start on fitness function

---
+++ Pink Salmon/model v.1/source/fitness.R
@@ -0,0 +1,13 @@
+fitness <- function(census, years){
+  years <- c(1:years)
+  
+  for( y in years){
+    census_parents <- data.frame(census[[i]])
+    census_juveniles <- data.frame(rbind(census[[i+1]], census[[i+2]], census[[i+3]]))
+    
+    
+    for(p in census_parents$fish.num){
+      
+    }
+  }
+  }
