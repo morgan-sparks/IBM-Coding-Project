3f599860472f84818732c0482a3f906e5f05b316
morgan-sparks <msparks1309@gmail.com>
2021-07-22 14:23:28 -0400

    add a bash submission script

---
+++ shell scripts/IBM_testrun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=Pink Salmon IBM test
+#SBATCH -A beagle
+#SBATCH -N 1
+#SBATCH -n 10
+#SBATCH --mem=20G
+#SBATCH -t 336:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=sparks35@purdue.edu
+
+cd $SLURM_SUBMIT_DIR
+
+module unload openmpi
+module purge
+
+module load gcc
+module load r
+
+R --no-save  < /scratch/bell/sparks35/IBM-Coding-Project/Pink\ Salmon/model\ v.1/lake\ population\ v1.R
+
+module purge
